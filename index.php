d4d691c8948e21710e33e70be630c801dcda5da6
CesarWatrin <cesar.watrin@hotmail.com>
2020-11-19 00:11:21 +0100

    Pages envoies

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
 // Boilerplate for exercice DAY 3 at university MIW 05
-// Code is bad and poor... but just enough for skills students :-) 
+// Code is bad and poor... but just enough for skills students :-)
 
 // Redirect to script to send email
 if (!empty($_POST['email'])) {
 
-    // Sending invitation by email
-    header('Location: send_email.php');
-    exit;
+   // Sending invitation by email
+   header('Location: send_email.php?email='.$_POST['email']);
+   exit;
 
 }
 
 $body_class = "";
 
-// Display delivery status, (tips anti-refreshing) 
+// Display delivery status, (tips anti-refreshing)
 if (!empty($_GET['delivery']) and $_GET['delivery'] === "sent") {
 
-    // Sending invitation by email
-    // echo "OK c'est envoyé..";
-    $body_class = "delivery_sent";
+   // Sending invitation by email
+   // echo "OK c'est envoyé..";
+   $body_class = "delivery_sent";
 }
 
 
@@ -27,22 +27,99 @@ if (!empty($_GET['delivery']) and $_GET['delivery'] === "sent") {
 <html lang="fr">
 
 <head>
+   <title>Chritmas Party</title>
+   <style type="text/css">
 
-    <title></title>
+   .delivery_sent .submit {
+      display: none;
+   }
 
-    <style type="text/css">
-        .delivery_sent {
-            background-color: green;
-        }
-    </style>
+   .delivery_sent .sended {
+      display: block;
+      background-color: rgba(255, 255, 255, 0.5);
+      text-align: center;
+      width: 400px;
+      height: 260px;
+      border-radius: 20px;
+      border: 2px solid #707070;
+   }
+
+   .delivery_sent .sended img {
+      width: 60%;
+      padding-top: 5%;
+      padding-left: 10%;
+   }
+
+   body {
+      background: url('img/background.png') no-repeat center center fixed;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+   }
+
+   html {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+   }
+
+   form {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      padding: 5%;
+   }
+
+   input:first-of-type {
+      width: 500px;
+      height: 70px;
+      margin-right: 20px;
+      border: 2px solid #B2B2B2;
+      border-radius: 5px 0 0 5px;
+      color: #8D8B8B;
+      text-align: left;
+      font-size: 25px;
+      padding-left: 30px;
+      font-weight: bold;
+   }
+
+   input:last-of-type {
+      width: 250px;
+      height: 76px;
+      border: 0;
+      border-radius: 0 5px 5px 0;
+      color: #ffffff;
+      background-color: rgba(0, 0, 0, 0);
+      background-image: url('img/send.png');
+      background-position: 180px;
+      background-repeat: no-repeat;
+      background-color: #C30078;
+      text-align: left;
+      font-size: 25px;
+      padding-left: 25px;
+      font-weight: bold;
+   }
+
+   .sended {
+      display: none;
+   }
+
+   </style>
 
 </head>
 <body class="<?= $body_class ?>">
 
-<form action="#" method="post">
-    <input type="email" name="email" placeholder="Ton email de star..." required/>
-    <input type="submit" value="Submit"/>
-</form>
+   <form action="#" method="post">
+      <div class="submit">
+         <input type="email" name="email" placeholder="Ton email de star..." required/>
+         <input type="submit" value="Inscris-toi !"/>
+      </div>
+      <div class="sended">
+         <img src="img/send2.png" alt="sended">
+      </div>
+   </form>
 
 </body>
 </html>
--- send_email.php
+++ send_email.php
@@ -12,34 +12,58 @@ require 'vendor/autoload.php';
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    //$mail->isSMTP();                                            // Send using SMTP
-    $mail->Host = 'mail.ovh.net';                    // Set the SMTP server to send through
-    $mail->SMTPAuth = true;                                   // Enable SMTP authentication
-    $mail->Username = 'VOTRE_LOGIN';                     // SMTP username
-    $mail->Password = 'PASSWORD';                               // SMTP password
-    //  $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-    //  $mail->Port       = 587;                                    // TCP port to connect to
+   //Server settings
+   // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+   // $mail->isSMTP();                                            // Send using SMTP
+   // $mail->Host = 'smtp.gmail.com';                    // Set the SMTP server to send through
+   // $mail->SMTPAuth = true;                                   // Enable SMTP authentication
+   // $mail->Username = 'guichetunique.chu@gmail.com';                     // SMTP username
+   // $mail->Password = 'hj8Kde65';                               // SMTP password
+   // // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+   // $mail->SMTPSecure = 'tls';
+   // $mail->Port       = 465;                                    // TCP port to connect to
 
-    //Recipients
-    $mail->setFrom('newsletter@miw.ovh', 'MIW Party');
-    $mail->addAddress('your-name@mix.org', 'Leon');     // Add a recipient
-    $mail->addCC('newsletter@miw.ovh');
+   // //Recipients
+   // $mail->setFrom('cesar.watrin@hotmail.com', 'MIW Party');
+   // $mail->addAddress($_GET['email']);     // Add a recipient
+   // $mail->addCC('cesar.watrin@hotmail.com');
+   //
+   // // Attachments
+   // //    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+   // //    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+   //
+   // // Content
+   // $mail->isHTML(true);                                  // Set email format to HTML
+   // $mail->Subject = 'Here is the subject';
+   // $mail->Body = file_get_contents('email/email_party.html');
+   // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+   //
+   // $mail->send();
 
-    // Attachments
-//    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+   $to = $_GET['email']; //$mailDemandeur
+   $from = 'guichetunique.chu@gmail.com';
+   $fromName = 'CHU Grenoble Alpes';
+   $subject = "Guichet Unique du CHUGA";
 
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body = file_get_contents('email/email_party.html');
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+   // Set content-type header for sending HTML email
+   $headers = 'From: '.$fromName.'<'.$from.'>' . "\r\n";
+   $headers .= "MIME-Version: 1.0" . "\r\n";
+   $headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
+   $headers .= "\r\n";
 
-    $mail->send();
-    //echo 'Message has been sent';
-    header('Location: index.php?delivery=sent');
+   // Additional headers
+   // $headers .= 'Cc: welcome@example.com' . "\r\n";
+   // $headers .= 'Bcc: welcome2@example.com' . "\r\n";
+
+   // Get HTML contents from file
+   $htmlContent = file_get_contents("email/email_party.html");
+   // $htmlContent = str_replace("maClefUnique", "$clef", $htmlContent);
+
+   // Send email
+   // mail($to, $subject, $htmlContent, $headers);
+
+   //echo 'Message has been sent';
+   header('Location: index.php?delivery=sent');
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+   echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
